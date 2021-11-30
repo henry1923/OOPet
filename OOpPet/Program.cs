9cec9a8e665d69eb7ec3524b871d8d1d1805bf56
Opilane TTHK <opilane@tthk.ee>
2021-11-30 15:48:34 +0200

    ok

---
+++ OOpPet/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace OOPPET
+{
+    class Program
+    {
+        class Pet
+        {
+            string name;
+            int age;
+            double weight;
+
+
+            public Pet(string _name)
+            {
+                name = name;
+                age = 0;
+                weight = 0.1;
+                Console.WriteLine($"Congratulations! You've adopted a {name}.");
+
+            }
+        }
+
+        public void Main(string[] args)
+        {
+            Console.WriteLine($"Name : {Peeter}");
+            Console.WriteLine($"Age = {3}");
+            Console.WriteLine($"Weight = {55kg}");
+
+        }
+
+        public void Manin(string[] args)
+        {
+            weight -= 0.1;
+            Console.WriteLine("spin-spin-spin");
+            284+87*
+        }
+
+        //destructor
+
+
+
+
+        static void Main(string[] args)
+        {
+            Pet myPet = new Pet("Hamster")
+             myPet.ShowPetInfo();
+            GC.Collect();
+
+
+
+
+        }
+    }
+}
+00/´õ´´äöö,
+    3
+
+'
+    4'õ
